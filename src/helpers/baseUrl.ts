05a292b1b4a1b222bbc4e7140598eac09e9d3d26
mehrnaz-akbari <mh.akbari@bluteam.ir>
2025-02-26 08:59:30 +0330

    dsds

--- src/helpers/baseUrl.ts
+++ src/helpers/baseUrl.ts
@@ -1,5 +1,1 @@
-import { read } from "@/helpers/storage";
-
-export const serverList = process.env?.NEXT_PUBLIC_BASE_URL?.split(",");
-
-export const baseUrl = read("baseUrl") || serverList?.[0];
+export const baseUrl = process.env?.NEXT_PUBLIC_BASE_URL;
