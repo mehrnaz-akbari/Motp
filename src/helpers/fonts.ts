83508c1cb9f08fc1951df596e0dae0bdb8555422
mehrnaz-akbari <mh.akbari@bluteam.ir>
2025-02-26 09:51:44 +0330

    dgdgd

--- src/helpers/fonts.ts
+++ src/helpers/fonts.ts
@@ -1,28 +1,26 @@
-
 import localFont from "next/font/local";
 
- export const geistIranYekan = localFont({
-    variable: "--font-iran-yekan",
-    src: [
-      {
-        path: "../assets/fonts/iranyekan/ttf/IRANYekanXFaNum-Regular.ttf",
-        weight: "700",
-        style: "normal",
-      },
-      {
-        path: "../assets/fonts/iranyekan/ttf/IRANYekanXFaNum-Bold.ttf",
-        weight: "900",
-      },
-      {
-        path: "../assets/fonts/iranyekan/ttf/IRANYekanXFaNum-Light.ttf",
-        weight: "300",
-        style: "normal",
-      },
-      {
-        path: "../assets/fonts/iranyekan/ttf/IRANYekanXFaNum-Regular.ttf",
-        weight: "500",
-        style: "normal",
-      },
-    ],
-  });
-  
+export const geistIranYekan = localFont({
+  variable: "--font-iran-yekan",
+  src: [
+    {
+      path: "../assets/fonts/iranyekan/ttf/IRANYekanMobileMedium.ttf",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../assets/fonts/iranyekan/ttf/IRANYekanMobileBold.ttf",
+      weight: "900",
+    },
+    {
+      path: "../assets/fonts/iranyekan/ttf/IRANYekanMobileLight.ttf",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "../assets/fonts/iranyekan/ttf/IRANYekanMobileRegular.ttf",
+      weight: "500",
+      style: "normal",
+    },
+  ],
+});
